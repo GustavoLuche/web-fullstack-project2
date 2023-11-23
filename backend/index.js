09f0c720384397175ba88d38bcb74a3559aae8a6
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-11-23 15:26:36 -0300

    Add server.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 // backend/index.js
 const express = require("express");
 const app = express();
-const port = 3001; 
+const port = 3001;
 
 // Define a route for the root URL ("/") that sends a simple message
 app.get("/", (req, res) => {
@@ -9,6 +9,4 @@ app.get("/", (req, res) => {
 });
 
 // Start the server and listen on the specified port
-app.listen(port, () => {
-  console.log(`Express server running on port ${port}`);
-});
+module.exports = app;
