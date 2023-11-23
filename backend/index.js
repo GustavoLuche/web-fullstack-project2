7155fc57dd3d903f1b4043445e9d3bbc2208a404
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-11-23 15:20:08 -0300

    Initialize Express Backend Environment

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+// backend/index.js
+const express = require("express");
+const app = express();
+const port = 3001; 
+
+// Define a route for the root URL ("/") that sends a simple message
+app.get("/", (req, res) => {
+  res.end("Express server running successfully!");
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Express server running on port ${port}`);
+});
