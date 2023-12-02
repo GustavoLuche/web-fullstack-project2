7437cf59972cb1fdd06f5dff9ca1f0db05076d00
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-12-02 10:13:18 -0300

    Transform Header into navbar with links
    Updated Header.js to a navbar structure with links for Insertion, Search, Notification, and a logout button.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,41 @@
 import React from "react";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { useAuthContext } from "../context/AuthContext";
 
-// O componente Header exibe um título na parte superior da aplicação.
 function Header({ title }) {
+  const { performLogout } = useAuthContext();
+
+  const handleLogout = () => {
+    performLogout();
+  };
+
   return (
-    <header className="App-header mb-3 py-2">
-      <h1 className="text-center">{title}</h1>
-    </header>
+    <Navbar expand="lg" className="App-header mb-3">
+      <Container className="mb-1">
+        <Navbar.Brand href="#home" className="text-white">
+          {title}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link className="text-white" href="#link">
+              Insertion
+            </Nav.Link>
+            <Nav.Link className="text-white" href="#link">
+              Search
+            </Nav.Link>
+            <Nav.Link className="text-white" href="#link">
+              Notification
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Collapse className="justify-content-end">
+          <Button variant="outline-danger" onClick={handleLogout}>
+            Logout
+          </Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
