f83dec2275702ba1e3708c357c7f142a927035bb
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-12-02 19:22:41 -0300

    Added HomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,15 @@
 import React from "react";
-import Header from "./components/Header";
-import Greeting from "./components/Greeting";
-import Search from "./components/Search";
-import SpinnerLoading from "./components/SpinnerLoading";
-import ErrorMessage from "./components/ErrorMessage";
-import SearchInfo from "./components/SearchInfo";
-import Paginator from "./components/Paginator";
-import AdviceList from "./components/AdviceList.js";
-import Footer from "./components/Footer";
-import { useAdviceContext } from "./context/AdviceContext";
+import HomePage from "./components/HomePage.js";
 import { useAuthContext } from "./context/AuthContext";
 import "./App.css";
 import Login from "./components/Login.js";
 
 function App() {
   // Usando o contexto para acessar o estado
-  const { state } = useAdviceContext();
   const { authState } = useAuthContext();
-  const { error, isLoading, searchTerm, searchPerformed } = state;
   const { isAuthenticated } = authState;
 
-  return (
-    <>
-      {!isAuthenticated ? (
-        <Login />
-      ) : (
-        <div className="App">
-          <Header title="Advice Slip Search" />
-          <Greeting />
-          <div
-            className={`App-container container ${
-              searchPerformed ? "" : "no-search-performed"
-            }`}
-          >
-            <Search />
-            {isLoading && <SpinnerLoading />}
-            {!isLoading && !error && searchTerm !== "" && (
-              <>
-                <SearchInfo />
-                <Paginator />
-                <AdviceList />
-              </>
-            )}
-            {error && !isLoading && <ErrorMessage />}
-          </div>
-          <Footer />
-        </div>
-      )}
-    </>
-  );
+  return <>{!isAuthenticated ? <Login /> : <HomePage />}</>;
 }
 
 export default App;
---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Header from "./Header";
+import Greeting from "./Greeting";
+import Search from "./Search";
+import SpinnerLoading from "./SpinnerLoading";
+import ErrorMessage from "./ErrorMessage";
+import SearchInfo from "./SearchInfo";
+import Paginator from "./Paginator";
+import AdviceList from "./AdviceList.js";
+import Footer from "./Footer";
+import { useAdviceContext } from "../context/AdviceContext.js";
+import "../App.css";
+
+function HomePage() {
+  // Usando o contexto para acessar o estado
+  const { state } = useAdviceContext();
+  const { error, isLoading, searchTerm, searchPerformed } = state;
+
+  return (
+    <div className="App">
+      <Header title="Advice Slip Search" />
+      <Greeting />
+      <div
+        className={`App-container container ${
+          searchPerformed ? "" : "no-search-performed"
+        }`}
+      >
+        <Search />
+        {isLoading && <SpinnerLoading />}
+        {!isLoading && !error && searchTerm !== "" && (
+          <>
+            <SearchInfo />
+            <Paginator />
+            <AdviceList />
+          </>
+        )}
+        {error && !isLoading && <ErrorMessage />}
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
