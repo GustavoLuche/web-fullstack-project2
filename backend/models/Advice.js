9779b0ea8bc87df874360204e8c51932d37105f9
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-12-04 22:31:59 -0300

    Add validations to Advice model

--- backend/models/Advice.js
+++ backend/models/Advice.js
@@ -10,6 +10,18 @@ const Advice = sequelize.define("Advice", {
   advice: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notNull: {
+        msg: "O campo 'advice' não pode ser nulo.",
+      },
+      notEmpty: {
+        msg: "O campo 'advice' não pode estar vazio.",
+      },
+      len: {
+        args: [1, 146], // Defina o comprimento máximo desejado
+        msg: "O conselho deve ter entre 1 e 146 caracteres.",
+      },
+    },
   },
 });
 
