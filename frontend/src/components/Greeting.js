b2f1143c98d74871410b60e86944049e25429e11
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-12-04 22:01:04 -0300

    Update Greeting.js

--- frontend/src/components/Greeting.js
+++ frontend/src/components/Greeting.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useAdviceContext } from "../context/AdviceContext.js";
 
 // O componente Greeting exibe uma mensagem de boas-vindas na aplicação.
 function Greeting() {
+  // Usando o contexto para acessar o estado
+  const { state } = useAdviceContext();
+  const { showSearchPage } = state;
+
   return (
     <div className="App-greeting mb-4">
-      <h2 className="text-center">Welcome to the Advice Slip Search</h2>
+      <h2
+        className={!showSearchPage ? "text-center" : "text-center text-danger"}
+      >
+        Welcome to the Advice Slip Search
+      </h2>
       <p className="text-center m-0">
         Get advice on various topics. Start by entering a search term below.
       </p>
